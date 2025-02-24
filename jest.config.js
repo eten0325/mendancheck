691a6e009e1380fce3d8823b63ce41ab0aa37b4e
eten0325 <ktmt0325@gmail.com>
2025-02-24 14:54:10 +0900

    Fix: TypeScript errors and Next.js recommendations　

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect','<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect','<rootDir>/jest.setup.ts'],
   setupFiles: ['./jest.setup.js'],
 };
